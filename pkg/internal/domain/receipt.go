494be814b2f31886ee5683d8e1f865859f786166
Тимур Ким <kepaliga@mail.ru>
2025-11-09 16:29:58 +0300

    comments newReceipt func

--- pkg/internal/domain/receipt.go
+++ pkg/internal/domain/receipt.go
@@ -39,58 +39,58 @@ type TariffCalc struct {
 	Step3Arithmetic float64
 }
 
-func NewPersonCalc(
-	placeNumber int,
-	FullName string,
-	Summ, Balance, Payment, Debt float64,
-	TariffName string,
-	CurrentInd, LastInd, DifValue float64,
-	Ratio int,
-	Step1Calc, Step2Calc, Step3Calc float64,
-	Step1Price, Step2Price, Step3Price float64,
-	Step1Arithmetic, Step2Arithmetic, Step3Arithmetic float64,
-) PersonCalc {
-	p := PersonCalc{
-		PlaceNumber: placeNumber,
-		FullName:    FullName,
-		Summ:        Summ,
-		Balance:     Balance,
-		Payment:     Payment,
-		Debt:        Debt,
-	}
+// func NewPersonCalc(
+// 	placeNumber int,
+// 	FullName string,
+// 	Summ, Balance, Payment, Debt float64,
+// 	TariffName string,
+// 	CurrentInd, LastInd, DifValue float64,
+// 	Ratio int,
+// 	Step1Calc, Step2Calc, Step3Calc float64,
+// 	Step1Price, Step2Price, Step3Price float64,
+// 	Step1Arithmetic, Step2Arithmetic, Step3Arithmetic float64,
+// ) PersonCalc {
+// 	p := PersonCalc{
+// 		PlaceNumber: placeNumber,
+// 		FullName:    FullName,
+// 		Summ:        Summ,
+// 		Balance:     Balance,
+// 		Payment:     Payment,
+// 		Debt:        Debt,
+// 	}
 
-	switch TariffName {
-	case "Тариф 1":
-		p.Single = &SingleZone{
-			Tariff: TariffCalc{
-				TariffName: TariffName,
-				CurrentInd: CurrentInd,
-				LastInd:    LastInd,
-				Step1Price: Step1Price,
-				Step2Price: Step2Price,
-				Step3Price: Step3Price,
-			},
-		}
-	default:
-		p.Duo = &DuoCalc{
-			T1: TariffCalc{
-				TariffName: TariffName,
-				CurrentInd: CurrentInd,
-				LastInd:    LastInd,
-				Step1Price: Step1Price,
-				Step2Price: Step2Price,
-				Step3Price: Step3Price,
-			},
-			T2: TariffCalc{
-				TariffName: TariffName,
-				CurrentInd: CurrentInd,
-				LastInd:    LastInd,
-				Step1Price: Step1Price,
-				Step2Price: Step2Price,
-				Step3Price: Step3Price,
-			},
-		}
-	}
+// 	switch TariffName {
+// 	case "Тариф 1":
+// 		p.Single = &SingleZone{
+// 			Tariff: TariffCalc{
+// 				TariffName: TariffName,
+// 				CurrentInd: CurrentInd,
+// 				LastInd:    LastInd,
+// 				Step1Price: Step1Price,
+// 				Step2Price: Step2Price,
+// 				Step3Price: Step3Price,
+// 			},
+// 		}
+// 	default:
+// 		p.Duo = &DuoCalc{
+// 			T1: TariffCalc{
+// 				TariffName: TariffName,
+// 				CurrentInd: CurrentInd,
+// 				LastInd:    LastInd,
+// 				Step1Price: Step1Price,
+// 				Step2Price: Step2Price,
+// 				Step3Price: Step3Price,
+// 			},
+// 			T2: TariffCalc{
+// 				TariffName: TariffName,
+// 				CurrentInd: CurrentInd,
+// 				LastInd:    LastInd,
+// 				Step1Price: Step1Price,
+// 				Step2Price: Step2Price,
+// 				Step3Price: Step3Price,
+// 			},
+// 		}
+// 	}
 
-	return p
-}
+// 	return p
+// }
